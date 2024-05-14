8333c5506754b510a395ba1368ccabc8df337d59
Alvaro Ku <alvaro.ku@grupoblueocean.com>
2024-05-13 19:16:02 -0600

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
                                     </svg>
                                 </div>
 
-                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Rola 2</h2>
+                                <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Automatic deploy</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                                     Alvaro Kú
